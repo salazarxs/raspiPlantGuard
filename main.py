ada7035f03c0c4994e91a0e0c9a9ce380eb98ef2
salazarxs <martinpaolo10@gmail.com>
2024-05-30 19:56:06 -0400

    .

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
-# flask --app main --debug run
-from flask import Flask,send_file,request
-from flask_socketio import SocketIO,emit
-from getEnvironmentData import GetEnvironmentData
-import asyncio 
-
+import asyncio
+import websockets
+from flask import Flask, request, send_file
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-socketio  =SocketIO(app)
 
+# Definir la función de servidor WebSocket
+async def websocket_server(websocket, path):
+    try:
+        while True:
+            message = await websocket.recv()
+            print(f"Mensaje recibido: {message}")
+            await websocket.send("Mensaje recibido por el servidor")
+    except websockets.exceptions.ConnectionClosedOK:
+        print("Conexión cerrada por el cliente")
 
-@app.route('/sendFiles',methods=['GET'])
-def index():
-    fileName  = request.args.get('filename')
-    print(fileName)
-    return send_file(f"./dayliData/{fileName}",as_attachment=True)
+# Configurar el servidor WebSocket
+start_websocket_server = websockets.serve(websocket_server, "localhost", 8765)
 
+# Definir las rutas HTTP
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+        return 'Archivo guardado correctamente'
+    else:
+        return 'No se ha enviado ningún archivo'
 
+@app.route('/download/<filename>', methods=['GET'])
+def download_file(filename):
+    try:
+        return send_file(filename, as_attachment=True)
+    except FileNotFoundError:
+        return 'Archivo no encontrado'
 
-@socketio.on('sendCurrentData')
-async def handleMessage(msg):
-    client_id = request.sid
-    while True:
-        currentData = GetEnvironmentData()
-        # Envía los datos al cliente utilizando el identificador único
-        emit('currentData', currentData, room=client_id)  
-        await asyncio.sleep(2)
-
-
-
-
-
-
+# Ejecutar ambos servidores en un bucle de eventos
 if __name__ == '__main__':
-     socketio.run(app) 
+    # Iniciar el servidor WebSocket
+    asyncio.get_event_loop().run_until_complete(start_websocket_server)
+    asyncio.get_event_loop().run_forever()
+
+    # Iniciar el servidor Flask
+    app.run(host='0.0.0.0', port=80)

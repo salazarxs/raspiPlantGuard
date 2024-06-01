c56ff0c5746448cb02e7f09ea6fc2adddba439f6
salazarxs <martinpaolo10@gmail.com>
2024-06-01 19:57:22 -0400

    .

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ async def websocket_server(websocket, path):
     except websockets.exceptions.ConnectionClosedOK:
         print("Conexión cerrada por el cliente")
 
-# Configurar el servidor WebSocket
-start_websocket_server = websockets.serve(websocket_server, "192.168.1.91", 8765)
-
 # Definir las rutas HTTP
 @app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
@@ -36,12 +33,17 @@ def run_flask():
 # Función asíncrona para guardar datos periódicamente
 async def periodic_save_data():
     while True:
-        data = GetEnvironmentData()
-        data_dict = json.loads(data)
-        humidity = data_dict["hum"]
-        temperature = data_dict["temp"]
-        sustrate_humidity = data_dict["sustrate_humidity"]
-        SaveData(humidity=humidity,soilHumidity=sustrate_humidity,temperature=temperature)
+        try:
+            data = GetEnvironmentData()
+            data_dict = json.loads(data)
+            humidity = data_dict["hum"]
+            temperature = data_dict["temp"]
+            sustrate_humidity = data_dict["sustrate_humidity"]
+            SaveData(humidity=humidity, soilHumidity=sustrate_humidity, temperature=temperature)
+        except RuntimeError as e:
+            print(f"Error al obtener datos del sensor: {e}. Reintentando en 5 segundos.")
+            await asyncio.sleep(5)  # Esperar 5 segundos antes de intentar nuevamente
+            continue
         await asyncio.sleep(300)  # Esperar 300 segundos (5 minutos) antes de guardar nuevamente
 
 # Ejecutar el bucle de eventos y ambos servidores en él
@@ -50,14 +52,20 @@ if __name__ == '__main__':
     flask_thread = threading.Thread(target=run_flask)
     flask_thread.start()
 
-    # Crear un bucle de eventos
-    loop = asyncio.get_event_loop()
+    # Crear un bucle de eventos nuevo para el servidor WebSocket y la tarea periódica
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
     # Ejecutar el servidor WebSocket en el bucle de eventos
-    asyncio.ensure_future(start_websocket_server)
+    loop.run_until_complete(websockets.serve(websocket_server, "192.168.1.91", 8765))
 
     # Ejecutar la tarea de guardado periódico en el bucle de eventos
-    asyncio.ensure_future(periodic_save_data())
+    loop.create_task(periodic_save_data())
 
     # Iniciar el bucle de eventos
-    loop.run_forever()
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
